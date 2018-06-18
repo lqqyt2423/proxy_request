47e91737c3b3fd718f060fe1366feabbd6a27357
liqiang <974923609@qq.com>
2018-06-18 16:12:52 +0800

    upgrade

--- index.js
+++ index.js
@@ -1,67 +1,114 @@
 'use strict';
 
-// node.js 中基于express 实现简单http代理服务
-const express = require('express');
-const morgan = require('morgan');
-const ip = require('ip');
-const url = require('url');
 const http = require('http');
+const https = require('https');
 const net = require('net');
-const request = require('request');
-const app = express();
-const config = {
-  port: 8888
+const url = require('url');
+
+const rp = {
+  http,
+  https,
 };
-const { port } = config;
 
-app.use(morgan('dev'));
+const server = http.createServer();
 
-// 代理HTTP 正常流量
-app.use((req, res) => {
+server.on('request', (req, res) => {
+  const method = req.method;
+  const headers = req.headers;
+  const path = req.url;
+  const startTime = Date.now();
+
+  // logger then res.end
+  const end = (body = '') => {
+    console.log(`${method} ${path} ${res.statusCode} ${Date.now() - startTime} ms - ${Buffer.byteLength(body)}`);
+    res.end(body);
+  };
+
+  // 因为是代理服务器，所以需要接收完整URI
+  // 如不是，直接报错
+  if (!/^http/.test(path)) {
+    res.statusCode = 404;
+    end('404\n');
+    return;
+  }
+
+  // console.log('method', method);
+  // console.log('headers', headers);
+  // console.log('path', path);
+  // console.log('status', res.statusCode);
+  // console.log('url', url.parse(path));
+
   const {
+    protocol = 'http:',
+    hostname,
+    port = 80,
+  } = url.parse(path);
+
+  // 重写headers的host属性
+  headers.host = hostname;
+
+  // 代理发出的客户端请求
+  // 防止接收到https请求，也做了处理
+  const proxyClient = rp[protocol.slice(0, -1)].request({
+    protocol,
+    hostname,
+    port,
     method,
-    headers
-  } = req;
+    headers,
+  }, (proxyRes) => {
+    const statusCode = proxyRes.statusCode;
+    const headers = proxyRes.headers;
 
-  // req => request => res
-  req.pipe(
-    request({
-      method,
-      headers,
-      uri: req.url,
-      // 表示解压缩数据
-      gzip: true
-    }, (error, response, body) => {
-      // body is the decompressed response body
-      // 此处可对返回数据进行处理等
-      // console.log(body);
-    }).on('response', response => {
-      // unmodified http.IncomingMessage object
-      res.writeHead(response.statusCode, response.headers);
-      response.pipe(res);
-    })
-  );
-});
+    // console.log('statusCode', statusCode);
+    // console.log('headers', headers);
 
-const server = http.createServer(app);
+    res.writeHead(statusCode, headers);
 
-// HTTP 隧道 代理HTTPS 流量 但由于加密 不能处理
-server.on('connect', (req, socket) => {
-  console.log('connect', req.url);
-  const parsedUrl = url.parse('http://' + req.url);
-  const { port, hostname } = parsedUrl;
-  const clientSocket = net.connect(port, hostname, () => {
-    socket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
-    clientSocket.pipe(socket);
-  }).on('error', e => {
-    console.log(e);
-    socket.end();
+    let body = [];
+    proxyRes.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    proxyRes.on('end', () => {
+      body = Buffer.concat(body);
+      end(body);
+    });
   });
-  socket.pipe(clientSocket);
+
+  // timeout 10s handler
+  proxyClient.setTimeout(10000, () => {
+    res.statusCode = 500;
+    end('500 timeout\n');
+  });
+
+  // error handler
+  proxyClient.on('error', (e) => {
+    res.statusCode = 500;
+    end(`500 error\n${e.message}\n`);
+  });
+
+  // 传输数据，发出请求
+  req.pipe(proxyClient);
 });
 
-// 开启server 监听指定端口
-server.listen(port, '0.0.0.0', () => {
-  // 打印当前ip 地址和port
-  console.log(`address: ${ip.address()}:${port}`);
+
+// ssl隧道
+server.on('connect', (req, socket, head) => {
+  const path = req.url;
+  // console.log('path', path);
+
+  const { port, hostname } = url.parse('http://' + path);
+
+  const proxyClient = net.createConnection(port, hostname, () => {
+    // 代理客户端socket连接建立后
+    console.log('ssl proxyClient connect', path);
+
+    socket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+
+    // 交换数据
+    proxyClient.write(head);
+    proxyClient.pipe(socket);
+    socket.pipe(proxyClient);
+  });
 });
+
+server.listen(8888);
