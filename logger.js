282de4470abe443ff4cdc30f00ad68a63038b9a0
lqqyt2423 <974923609@qq.com>
2019-11-13 19:51:51 +0800

    变为lib形式

--- logger.js
+++ logger.js
@@ -4,4 +4,14 @@ const { EggConsoleLogger } = require('egg-logger');
 
 const logger = new EggConsoleLogger({ level: 'DEBUG' });
 
+['debug', 'info', 'warn', 'error'].forEach(level => {
+  const fn = logger[level];
+  logger[level] = function(...args) {
+    if (args.length && typeof args[0] === 'string') {
+      args[0] = '[proxy_request] ' + args[0];
+    }
+    fn.call(logger, ...args);
+  };
+});
+
 module.exports = logger;
