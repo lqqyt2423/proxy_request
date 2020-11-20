7a08c23a5173c9c42d2d7cec2d486ccdda479da2
lqqyt2423 <974923609@qq.com>
2020-11-20 17:36:12 +0800

    ModifyHandler directResponse

--- src/example.ts
+++ src/example.ts
@@ -1,15 +1,40 @@
+import { PassThrough } from 'stream';
 import { FwProxy } from '.';
+import { Interpolator, IRequest, IResponse } from './interpolator';
 import { Logger } from './logger';
-import { LogViewer } from './viewer';
+import { FileViewer, LogViewer } from './viewer';
+
+class TestInterpolator implements Interpolator {
+    public name = 'TestInterpolator';
+
+    public async directResponse(req: IRequest): Promise<IResponse> {
+        if (req.url.includes('localhost')) {
+            // 模拟读了 req 中的请求流
+            // req.body.resume();
+
+            const pass = new PassThrough();
+            pass.end('hello world\n');
+            return {
+                statusCode: 200,
+                headers: {
+                    'proxy-interpolator': this.name,
+                },
+                body: pass,
+            };
+        }
+
+        return null;
+    }
+}
 
 const logger = new Logger('FwProxy example');
 
 const fwproxy = new FwProxy({ interceptHttps: true, verbose: true });
 
 fwproxy.addViewer(new LogViewer());
+fwproxy.addViewer(new FileViewer(process.stdout));
 
-// TODO
-fwproxy.addInterpolator({ name: 'test interpolator' });
+fwproxy.addInterpolator(new TestInterpolator());
 
 fwproxy.on('error', err => {
     logger.error(err);
--- src/modify-handler.ts
+++ src/modify-handler.ts
@@ -1,3 +1,4 @@
+import { PassThrough } from 'stream';
 import { FwProxy } from '.';
 import { Interpolator, IRequest, IResponse } from './interpolator';
 import { Logger } from './logger';
@@ -6,13 +7,13 @@ export class ModifyHandler {
     private fwproxy: FwProxy;
     private logger: Logger;
 
-    private changeResponseFns: Array<(req: IRequest, rawRes: IResponse) => Promise<IResponse>>;
+    private interps: Array<Interpolator>;
 
     constructor(fwproxy: FwProxy) {
         this.fwproxy = fwproxy;
         this.logger = new Logger('FwProxy ModifyHandler');
 
-        this.changeResponseFns = [];
+        this.interps = [];
     }
 
     public async isParseSecure(url: string): Promise<boolean> {
@@ -20,7 +21,35 @@ export class ModifyHandler {
     }
 
     public async directResponse(req: IRequest): Promise<{ reqInfo: IRequest, resInfo: IResponse }> {
-        return null;
+        if (!this.interps.length) return null;
+
+        const targetInterps = this.interps.filter(interp => !!interp.directResponse);
+        if (!targetInterps.length) return null;
+
+        // 监测用户端是否消耗了请求流 req.body，如果消耗了则需要拷贝请求流
+        let pass: PassThrough;
+        const resumeCallback = () => {
+            pass = new PassThrough();
+            req.body.pipe(pass);
+        };
+        req.body.once('resume', resumeCallback);
+
+        // 假如用户添加了多个 directResponse 方法，且再第一个方法中消耗了请求流，则后面几个方法中请求流为空
+        let resInfo: IResponse;
+        for (let i = 0, len = targetInterps.length; i < len; i++) {
+            resInfo = await targetInterps[i].directResponse(req);
+            if (resInfo) break;
+        }
+
+        if (pass) {
+            this.logger.debug('directResponse 拷贝流');
+            req.body = pass;
+        } else {
+            this.logger.debug('directResponse 保留流');
+            req.body.removeListener('resume', resumeCallback);
+        }
+
+        return { reqInfo: req, resInfo };
     }
 
     public async changeRequest(req: IRequest): Promise<IRequest> {
@@ -28,11 +57,13 @@ export class ModifyHandler {
     }
 
     public async changeResponse(req: IRequest, rawRes: IResponse): Promise<IResponse> {
-        if (!this.changeResponseFns.length) return null;
+        if (!this.interps.length) return null;
 
         let res: IResponse = rawRes;
-        for (let i = 0, len = this.changeResponseFns.length; i < len; i++) {
-            res = await this.changeResponseFns[i](req, res);
+        for (let i = 0, len = this.interps.length; i < len; i++) {
+            if (this.interps[i].changeResponse) {
+                res = await this.interps[i].changeResponse(req, res);
+            }
         }
 
         return res;
@@ -40,9 +71,6 @@ export class ModifyHandler {
 
     public add(interpolator: Interpolator) {
         this.logger.show('添加 interpolator: %s', interpolator.name);
-
-        if (interpolator.changeResponse) {
-            this.changeResponseFns.push(interpolator.changeResponse.bind(interpolator));
-        }
+        this.interps.push(interpolator);
     }
 }
